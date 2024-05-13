c413179f55bb8196c3b1f65d526bf87329d07825
Isabel Martinez <isabelmartinez19.30@gmail.com>
2024-05-13 17:28:17 -0400

    feat: registro de usuario

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,14 +1,13 @@
-import React from "react";
-import "./Register.css";
-import Form from "../form/Form";
-import { Link } from "react-router-dom";
+import './Register.css';
+import Form from '../form/Form';
+import { Link } from 'react-router-dom';
 
 function Register() {
   return (
     <section className="register">
-      <Form title="Inscribirse">
+      <Form action="signup" title="Inscribirse">
         <p className="register_paragraph">
-          ¿Ya eres miembro? Inicia{" "}
+          ¿Ya eres miembro? Inicia{' '}
           <Link to="/signin" className="register__link">
             sesión aquí
           </Link>
