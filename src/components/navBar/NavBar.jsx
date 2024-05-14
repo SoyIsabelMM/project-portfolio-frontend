ebce209b1e16b4ff6a7864adbaca0bd2ec9fcfe8
Mary Martinez <isa@MacBook-Air-de-Mary.local>
2024-05-14 16:18:30 -0400

    refactor: Crear lógica de ocultar menú

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -5,38 +5,51 @@ import './NavBar.css';
 
 function NavBar() {
   const { currentUser } = useContext(CurrentUserContext);
+  const isLoggedIn = currentUser !== null;
+  const hasSelectedProfile = isLoggedIn && currentUser._id;
 
   return (
     <div className="nav-bar">
       <ul className="nav-bar__list">
-        <li className="nav-bar__options">
-          <NavLink
-            className="nav-bar__ref"
-            to={`/profile/${currentUser ? currentUser._id : ''}`}
-          >
-            Perfil
-          </NavLink>
-        </li>
-        <li className="nav-bar__options">
-          <NavLink className="nav-bar__ref" to="/portfolios">
-            Portafolios
-          </NavLink>
-        </li>
-        <li className="nav-bar__options">
-          <NavLink className="nav-bar__ref" to="/about-me">
-            Acerca de
-          </NavLink>
-        </li>
-        <li className="nav-bar__options">
-          <NavLink className="nav-bar__ref" to="/contact">
-            Contacto
-          </NavLink>
-        </li>
-        <li className="nav-bar__options">
-          <NavLink className="nav-bar__ref" to="/edit-info">
-            Editar Perfil
-          </NavLink>
-        </li>
+        {isLoggedIn && hasSelectedProfile && (
+          <li className="nav-bar__options">
+            <NavLink
+              className="nav-bar__ref"
+              to={`/profile/${currentUser._id}`}
+            >
+              Perfil
+            </NavLink>
+          </li>
+        )}
+        {isLoggedIn && hasSelectedProfile && (
+          <li className="nav-bar__options">
+            <NavLink className="nav-bar__ref" to="/portfolios">
+              Portafolios
+            </NavLink>
+          </li>
+        )}
+
+        {isLoggedIn && hasSelectedProfile && (
+          <li className="nav-bar__options">
+            <NavLink className="nav-bar__ref" to="/about-me">
+              Acerca de
+            </NavLink>
+          </li>
+        )}
+        {isLoggedIn && hasSelectedProfile && (
+          <li className="nav-bar__options">
+            <NavLink className="nav-bar__ref" to="/contact">
+              Contacto
+            </NavLink>
+          </li>
+        )}
+        {isLoggedIn && (
+          <li className="nav-bar__options">
+            <NavLink className="nav-bar__ref" to="/edit-info">
+              Editar Perfil
+            </NavLink>
+          </li>
+        )}
       </ul>
     </div>
   );
