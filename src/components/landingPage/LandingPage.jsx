f63a729de9bab0aa8d9fe8b7dc9c9cab7ee6c890
Isabel Martinez <isabelmartinez19.30@gmail.com>
2024-05-15 20:56:50 -0400

    feat: cargar perfiles desde el backend en landing page

--- src/components/landingPage/LandingPage.jsx
+++ src/components/landingPage/LandingPage.jsx
@@ -1,14 +1,33 @@
-import React, { useState } from "react";
-import "./LandingPage.css";
-import icon from "../../images/logo-miniatura.png";
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-function LandingPage({ children, onClick, onSearch }) {
-  const [query, setQuery] = useState("");
+import './LandingPage.css';
+import icon from '../../images/logo-miniatura.png';
+import { fetchProfiles } from '../../utils/userApi';
+import ProfileCard from '../profileCard/ProfileCard';
+
+function LandingPage({ onClick, onSearch }) {
+  const [query, setQuery] = useState('');
+  const [profiles, setProfiles] = useState([]);
 
   const handleSearchBtnClick = () => {
-    console.log("Buscar fotos con la palabra clave:", query);
+    console.log('Buscar fotos con la palabra clave:', query);
     onSearch(query);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const profiles = await fetchProfiles();
+
+        setProfiles(profiles);
+      } catch (err) {
+        console.error('Error fetching profiles:', err);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <section className="landing-page">
@@ -29,12 +48,23 @@ function LandingPage({ children, onClick, onSearch }) {
           Buscar
         </button>
       </div>
-      <div className="landing-page__cards">{children}</div>
+
+      <div className="landing-page__cards">
+        {profiles.map((profile, index) => (
+          <ProfileCard key={index} profile={profile} />
+        ))}
+      </div>
+
       <button className="landing-page__btn-see" onClick={onClick}>
         Ver más perfiles
       </button>
     </section>
   );
 }
+
+LandingPage.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  onSearch: PropTypes.func.isRequired,
+};
 
 export default LandingPage;
